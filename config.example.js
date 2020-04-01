3ac5f72b9b58d7378758698ec21e6c9d49144cf4
sagar608 <sagarkarki076@gmail.com>
2020-04-01 08:20:23 +0545

    added key

---
+++ config.example.js
@@ -0,0 +1,7 @@
+const $ = selector =>  document.querySelector(selector);
+//api key
+const apiKey = "5cdbab9eb56a033234a450178bc4143c";
+const weatherContainer = $('.weather-container');
+
+const notificationElement = $('.notification');
+const input = $('.input-control input')
